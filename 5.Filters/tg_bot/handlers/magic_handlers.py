1aea48f6cb0ff4b68aca6dc89dd7e21c834eaf1b
Pushistost <arlant@mail.ru>
2024-03-02 00:07:26 +0100

    add accept_from_user function and several id, who is welcomed as creator

--- 5.Filters/tg_bot/handlers/magic_handlers.py
+++ 5.Filters/tg_bot/handlers/magic_handlers.py
@@ -6,3 +6,8 @@ magic_router = Router()
 @magic_router.message(F.photo[-1].as_("photo"))
 async def accept_photo(message: types.Message, photo: types.PhotoSize):
     await message.answer(f"Photo: {photo}")
+
+
+@magic_router.message(F.from_user.id.in_([258829722, 32332323, 62789002, 312313412, 24241244]))
+async def accept_from_user(message: types.Message):
+    await message.answer(f"Hello my creator!")
